697306a0f3b3a04f85cd3f9155876fa552a96183
Jemienachukwu <jemieofoegbu@gmail.com>
2023-03-16 15:28:39 +0100

    auth import fix

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,5 @@
 import express from "express";
-import { admin, protect } from "../middleware/authMiddleware.js";
-
+admin;
 import {
   createProduct,
   deleteProduct,
@@ -10,6 +9,7 @@ import {
   reviewProduct,
   getTopProucts,
 } from "../controllers/productController.js";
+import { admin, protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
