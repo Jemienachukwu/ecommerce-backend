342f56d10ecc227c047e46a2127d7602d2b0b782
Jemienachukwu <jemieofoegbu@gmail.com>
2023-03-25 23:24:10 +0100

    added cors middleware

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
