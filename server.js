2395c9ba4a3a82b0ac2921d92c91faaf1317fe79
Jemienachukwu <jemieofoegbu@gmail.com>
2023-03-25 23:57:48 +0100

    update

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ connectDB();
 
 const app = express();
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://techshop-fiqo.onrender.com"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
