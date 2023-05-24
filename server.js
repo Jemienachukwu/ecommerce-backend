de0eeeb7b912b90bc271bf63922c1f60548ea86d
Jemienachukwu <jemieofoegbu@gmail.com>
2023-05-24 23:26:46 +0100

    update

--- server.js
+++ server.js
@@ -15,17 +15,17 @@ connectDB();
 
 const app = express();
 
-// app.use(function (req, res, next) {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "https://techshop-fiqo.onrender.com"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://techshop-fiqo.onrender.com"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
